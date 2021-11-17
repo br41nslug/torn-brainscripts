ce5e502820bc705797e758d2c533bb26f7abea2c
brainslug <tim@brainslug.nl>
2021-11-18 00:26:44 +0100

    added full height driver display "script"

---
+++ racing-full-height.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         BrainRacing: Full Height Driver Panel
+// @namespace    brainslug.torn.racing
+// @version      0.2
+// @description  Simply shows the full height driver panel nothing more nothing less
+// @author       Brainslug [2323221]
+// @match        https://www.torn.com/loader.php?sid=racing*
+// @icon         https://www.google.com/s2/favicons?domain=torn.com
+// @updateURL    https://raw.githubusercontent.com/br41nslug/torn-brainscripts/main/racing-full-height.user.js
+// @grant        GM_addStyle
+// ==/UserScript==
+GM_addStyle(`
+.d .racing-main-wrap .car-selected-wrap #drivers-scrollbar#drivers-scrollbar {
+    max-height: fit-content!important;
+}
+`);
