471269257792fe723e3411d73c78f46743ce10d5
brainslug <tim@brainslug.nl>
2021-11-18 00:35:46 +0100

    added always on top racing "script"

---
+++ racing-always-on-top.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         BrainRacing: Always On Top
+// @namespace    brainslug.torn.racing
+// @version      0.2
+// @description  Pull you to the top so you don't have to go looking for your position in big races
+// @author       Brainslug [2323221]
+// @match        https://www.torn.com/loader.php?sid=racing*
+// @icon         https://www.google.com/s2/favicons?domain=torn.com
+// @updateURL    https://raw.githubusercontent.com/br41nslug/torn-brainscripts/main/racing-always-on-top.user.js
+// @grant        GM_addStyle
+// ==/UserScript==
+
+const user_id = document.cookie.match('(^|;)\\s*uid\\s*=\\s*([^;]+)')?.pop() || '';
+
+GM_addStyle(`
+#leaderBoard {
+  padding-top: 32px;
+  position: relative;
+}
+#lbr-${user_id} {
+  position: absolute;
+  width: 100%;
+  top: 0;
+}
+`);
