78ea094f92735c502ba6513450993bf7c2efe266
brainslug <tim@brainslug.nl>
2021-11-21 18:09:40 +0100

    added nice class banner selector to the race banner script

--- racing-change-banner.user.js
+++ racing-change-banner.user.js
@@ -1,26 +1,71 @@
 // ==UserScript==
 // @name         BrainRacing: Change Racing Banner
 // @namespace    brainslug.torn.racing
-// @version      0.2
+// @version      0.4
 // @description  Allows you to pick different race class decoration
 // @author       Brainslug [2323221]
 // @match        https://www.torn.com/loader.php?sid=racing*
 // @icon         https://www.google.com/s2/favicons?domain=torn.com
 // @updateURL    https://raw.githubusercontent.com/br41nslug/torn-brainscripts/main/racing-change-banner.user.js
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addStyle
 // ==/UserScript==
 
-const SELECTED_CLASS = 'C';
-const CURRENT_CLASS = 'A';
+const raceClasses = ['A', 'B', 'C', 'D', 'E'];
+let selectedBanner = GM_getValue("BR_CRB_BANNER", "A");
+let $tornBanner, $classDropdown;
+
+function changeBanner(raceClass) {
+    if (!$tornBanner) return;
+    raceClasses.forEach(function (cls) {
+        if ($tornBanner.hasClass('class-'+cls)) {
+            $tornBanner.removeClass('class-'+cls);
+        }
+    });
+    $tornBanner.addClass("class-" + raceClass);
+    GM_setValue("BR_CRB_BANNER", raceClass);
+}
+
+function addSelector() {
+    $classDropdown = $(`<div class="react-dropdown-default change-banner">
+    <button type="button" class="toggler down">Change Banner</button>
+    <div class="dropdownList down">
+        <ul class="scrollarea scroll-area dropdown-content scrollbar-bright down"></ul>
+    </div>
+</div>`);
+    $classDropdown.on('click', function () {
+        $classDropdown.toggleClass('dropdown-opened');
+    });
+    let $list = $classDropdown.find('ul');
+    raceClasses.forEach(function (cls) {
+        let $item = $(`<li class="item">Class ${cls}</li>`);
+        $item.on('click', function () {
+            changeBanner(cls);
+        });
+        $list.append($item);
+    });
+    GM_addStyle(`
+    .change-banner {
+        float: left;
+        margin-top: -4px;
+        margin-left: 10px;
+    }
+    .content-title .change-banner button.toggler.toggler {
+        padding-left: 10px;
+    }`);
+    $('#skip-to-content').after($classDropdown);
+}
 
 (function() {
     'use strict';
     const maxLoop = 10000;
     let counter = 1;
     let loop = setInterval(function () {
-        let $elem = $('.racing-main-wrap.class-'+CURRENT_CLASS);
-        if ($elem.length > 0 || counter > maxLoop) {
-            $elem.removeClass('class-'+CURRENT_CLASS).addClass('class-'+SELECTED_CLASS);
+        $tornBanner = $('.racing-main-wrap');
+        if ($tornBanner.length > 0 || counter > maxLoop) {
+            changeBanner(selectedBanner);
+            addSelector();
             return clearInterval(loop);
         }
         counter++;
