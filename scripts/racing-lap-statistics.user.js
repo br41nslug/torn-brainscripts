83e040e7807eef1c7b9f70dd6024d4b3634d1221
brainslug <tim@brainslug.nl>
2022-01-29 22:35:34 +0100

    integrated charts script into lap statistics

--- scripts/racing-lap-statistics.user.js
+++ scripts/racing-lap-statistics.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         BrainRacing: Extra Lap Statistics
 // @namespace    brainslug.torn.racing
-// @version      0.3.2
+// @version      0.4.0
 // @description  Removing the useless left sidebar and adding statistics on the right!
 // @author       Brainslug [2323221]
 // @match        https://www.torn.com/loader.php?sid=racing*
 // @icon         https://www.google.com/s2/favicons?domain=torn.com
 // @updateURL    https://raw.githubusercontent.com/br41nslug/torn-brainscripts/main/scripts/racing-lap-statistics.user.js
+// @require      https://raw.githubusercontent.com/br41nslug/torn-brainscripts/main/lib/chart.min.js
 // @grant        GM_addStyle
 // @run-at       document-body
 // ==/UserScript==
@@ -14,9 +15,11 @@
 // init
 console.info('[BrainRacing]', 'Extra lap statistics');
 interceptRaceData(function (data) {
+    data = decodeData(data);
     const raceData = processRaceData(data);
     $("#racingupdatesnew").ready(function () {
         injectSidebar();
+        injectCharts(data);
         updateLeaderboard('', 0, parseLapData(raceData, 0));
         watchRaceBar(function (name, lap) {
             console.debug('[BrainRacing] updateLeaderboard', name, lap);
@@ -78,8 +81,116 @@ GM_addStyle(`
 .br-leaderboard-listitem .value:last-child {
     width: 50px;
 }
+.stats-icon {
+    margin: 8px 10px;
+    height: 13px;
+    width: 16px;
+    float: right;
+    background: url(/images/v2/personal_stats/stats.png) 0 0 no-repeat;
+    filter: drop-shadow(0px 0px 1px #11111194);
+}
+.b-modal {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #f2f2f2;
+    border-right: 1px solid #ababab;
+    border-bottom: 1px solid #ababab;
+    border-left: 1px solid #ababab;
+    border-radius: 5px;
+    color: #333;
+    z-index: 99999;
+    -webkit-box-shadow: 4px 4px 12px 4px rgba(0,0,0,0.75);
+    -moz-box-shadow: 4px 4px 12px 4px rgba(0,0,0,0.75);
+    box-shadow: 4px 4px 12px 4px rgba(0,0,0,0.75);
+}
+.b-modal.hidden {
+    display: none;
+}
+.b-modal .b-header {
+    position: relative;
+    height: 34px;
+    margin: -1px 5px 0;
+    background: url(/images/v2/chat/tabs_middle.png) 0 0 repeat-x;
+}
+.b-modal .b-header .b-inner {
+    display: flex;
+}
+.b-modal .b-header .b-corner {
+    position: absolute;
+    top: 0;
+    height: 34px;
+    width: 7px;
+    background: url(/images/v2/chat/tabs_ends.png) 0 0 no-repeat;
+}
+.b-modal .b-header .b-corner.b-left {
+    left: -6px;
+}
+.b-modal .b-header .b-corner.b-right {
+    right: -6px;
+    background-position: 100% 0;
+}
+.b-modal .b-header .b-icon {
+    margin-left: -6px;
+    margin-top: 8px;
+    height: 16px;
+    width: 34px;
+    vertical-align: middle;
+    background: url(/images/v2/personal_stats/stats.png) 10px 3px no-repeat;
+    filter: drop-shadow(0 0 1px #111111AD);
+}
+.b-modal .b-header .b-title {
+    flex-grow: 1;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    color: #fff;
+    line-height: 34px;
+    font-weight: bold;
+}
+.b-modal .b-header .b-export,
+.b-modal .b-header .b-clear {
+    padding-right: 16px;
+    cursor: pointer;
+    color: #e3e3e3;
+    line-height: 34px;
+}
+.b-modal .b-header .b-export:hover,
+.b-modal .b-header .b-clear:hover {
+    text-decoration: underline;
+}
+.b-modal .b-header .b-close {
+    width: 16px;
+    background: url(/images/v2/chat/tab_icons.svg) -552px top;
+    filter: drop-shadow(0 0 1px #111111AD);
+    vertical-align: middle;
+    height: 34px;
+    cursor: pointer;
+}
+.b-modal .b-header .b-close:hover {
+    background-position: -552px bottom;
+}
+.b-modal .b-content {
+    padding: 16px;
+}
 `);
 
+// chart
+function showChart(data, options) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var conf = {
+        type: 'line',
+        data: data,
+        options: options
+    };
+    //console.log('chart', conf);
+    return new Chart(ctx, conf);
+}
+
+// race colors
+const race_colors = ["#4c6600","#b20000","#b28500","#005b5b","#003366","#46008c","#660066","#000000","#f28d8d","#e1c919","#a0cf17","#18d9d9","#6fafee","#b072ef","#f080f0","#616161","#400000","#403000","#204000","#003040","#000040","#400040"];
+
+// get the data for a single lap
 function parseLapData(results, lap) {
     if (lap < 1) return results[0].positions.map(playername => ([playername, 0, 0, 0 ]));
     const lapData = results[lap - 1];
@@ -114,15 +225,21 @@ function pad(num, size) {
     return ('000000000' + num).substr(-size);
 }
 
-// process race data
-function processRaceData(data) {
+// decode data
+function decodeData(data) {
     const carsData = data.raceData.cars;
-    const trackIntervals = data.raceData.trackData.intervals.length;
-    const raceData = Object.keys(carsData).reduce((result, playername) => {
+    data.raceData.cars = Object.keys(carsData).reduce((result, playername) => {
         result[playername] = decode64(carsData[playername])
             .split(',').map((nr) => Number(nr));
         return result;
     }, {});
+    return data;
+}
+
+// process race data
+function processRaceData(data) {
+    const carsData = data.raceData.cars;
+    const trackIntervals = data.raceData.trackData.intervals.length;
     let crashed = []; // skip future loops for this player
     let times = {}; // cumulative player times
     let result = [];
@@ -132,13 +249,13 @@ function processRaceData(data) {
         for (const playername in carsData) {
             if (!times[playername]) times[playername] = 0;
             if (crashed.includes(playername)) continue;
-            if (raceData[playername].length < (l * trackIntervals + trackIntervals)) {
+            if (carsData[playername].length < (l * trackIntervals + trackIntervals)) {
                 crashed.push(playername);
                 continue;
             }
             let lapTime = 0;
             for (let i = 0; i < trackIntervals; i++) {
-                lapTime +=  raceData[playername][l * trackIntervals + i]
+                lapTime +=  carsData[playername][l * trackIntervals + i]
             }
             lapTime = Number(lapTime.toPrecision(4));
             lapTimes[playername] = lapTime;
@@ -167,6 +284,72 @@ function processRaceData(data) {
     return result;
 }
 
+// process chart data
+function processChartData(data) {
+    const carsData = data.raceData.cars;
+    const trackIntervals = data.raceData.trackData.intervals.length;
+    // calc the positions
+    let times = {}; // cumulative player times
+    let positions = [];
+    for (let s = 0; s < trackIntervals * data.laps; s++) {
+        let segment = [];
+        for (const playername in carsData) {
+            if (!times[playername]) times[playername] = 0;
+            if (!carsData[playername][s]) continue;
+            times[playername] += carsData[playername][s];
+            segment.push([playername, times[playername]]);
+        }
+        positions.push(segment.sort((a,b) => (a[1]-b[1])).map(x => x[0]));
+    }
+    // invert the positions to per player
+    const playerPosition = positions.reduce((res, pos) => {
+        pos.forEach((player, index) => {
+            if (!res[player]) res[player] = [];
+            res[player].push(index + 1);
+        });
+        return res;
+    }, {});
+    // compress the player positions
+    const playerPositionCompressed = Object.keys(playerPosition).reduce((result, player) => {
+        let pos = [];
+        const seglen = playerPosition[player].length;
+        for (let s = 0; s < seglen; s++) {
+            if (s > 0 && s < seglen - 1) {
+                if (playerPosition[player][s] != playerPosition[player][s + 1] ||
+                    playerPosition[player][s] != playerPosition[player][s - 1]) {
+                    pos.push([
+                        s / trackIntervals,
+                        playerPosition[player][s]
+                    ]);
+                }
+            } else {
+                pos.push([
+                    s / trackIntervals,
+                    playerPosition[player][s]
+                ]);
+            }
+        }
+        result[player] = pos;
+        return result;
+    }, {});
+    //console.info('[bs] chart3', playerPositionCompressed);
+    // build datasets
+    const colors = Object.values(data.raceData.carInfo).reduce(function (acc, cur) {
+        acc[cur.playername] = race_colors[cur.color - 1];
+        return acc;
+    }, {});
+    const datasets = Object.keys(playerPositionCompressed).map(function (player) {
+        return {
+            label: player,
+            data: playerPositionCompressed[player].map(([x, y]) => ({ x, y })),
+            borderColor: colors[player],
+            stepped: true,
+        };
+    });
+    console.info('[bs] chart4', datasets);
+    return datasets;
+}
+
 // watch DOM
 function watchRaceBar(callback, timeout=100) {
     let lap = -1, name = '';
@@ -183,6 +366,53 @@ function watchRaceBar(callback, timeout=100) {
         }
     }, timeout);
     return () => clearInterval(loop);
+}
+
+// inject chart link
+function injectCharts(data) {
+    if ($('.b-modal').length <= 0) {
+        // inject modal
+        $('body').append(`<div class="b-modal hidden">
+            <div class="b-header">
+                <div class="b-inner">
+                    <div class="b-icon"></div>
+                    <div class="b-title">Brainslug's Position Statistics</div>
+                    <span class="b-clear">[ clear ]</span><div class="b-close"></div>
+                </div>
+                <div class="b-corner b-left"></div><div class="b-corner b-right"></div>
+            </div>
+            <div class="b-content"><canvas id="myChart" width="800" height="600"></canvas></div>
+        </div>`);
+        // inject button
+        $('.track-info-wrap').parent().append('<div class="track-info-wrap" id="brain-charts"><i class="stats-icon"></i></div>');
+        // bind events
+        let chart = false;
+        $('#brain-charts,.b-modal .b-close').on('click', function () {
+            if ($('.b-modal').hasClass('hidden')) {
+                $('.b-modal').removeClass('hidden');
+                if (!chart) {
+                    chart = showChart({ labels: new Array(50), datasets: processChartData(data) }, {
+                        scales: {
+                            y: { reverse: true, ticks: {
+                                callback: (value) => (value % Math.floor(value)) !== 0 ? '' : Math.floor(value)
+                            } },
+                            x: { type: 'linear', ticks: {
+                                callback: (value) => (value % Math.floor(value)) !== 0 ? '' :'lap '+value
+                            } }
+                        }
+                    });
+                }
+            } else {
+                $('.b-modal').addClass('hidden');
+            }
+        });
+        $(".b-modal .b-clear").click(function() {
+            chart.data.datasets.forEach(function(ds) {
+                ds.hidden = !ds.hidden;
+            });
+            chart.update();
+        });
+    }
 }
 
 // inject sidebar
