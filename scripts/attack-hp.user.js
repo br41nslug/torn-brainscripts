2b72ab1e2d09ccb5677214dacd501137af001e57
Brainslug <tim@brainslug.nl>
2024-09-13 21:52:33 +0200

    Added show HP script and made stealth script compatible

--- scripts/attack-hp.user.js
+++ scripts/attack-hp.user.js
@@ -1,25 +1,27 @@
 // ==UserScript==
-// @name         BrainTools: Show Stealth
+// @name         BrainTools: Show HP%
 // @namespace    brainslug.torn.tools
-// @version      0.3
+// @version      0.1
 // @description  Shows the stealth percentage in the header.
 // @author       Brainslug [2323221]
 // @match        https://www.torn.com/loader.php?sid=attack&user2ID=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
-// @downloadURL  https://raw.githubusercontent.com/br41nslug/torn-brainscripts/main/scripts/attack-stealth.user.js
-// @updateURL    https://raw.githubusercontent.com/br41nslug/torn-brainscripts/main/scripts/attack-stealth.user.js
+// @downloadURL  https://raw.githubusercontent.com/br41nslug/torn-brainscripts/main/scripts/attack-hp.user.js
+// @updateURL    https://raw.githubusercontent.com/br41nslug/torn-brainscripts/main/scripts/attack-hp.user.js
 // @grant        none
 // ==/UserScript==
 
-function watchPercentage(target, title) {
+function watchPercentages(attacker, defender, title) {
     function updatePercentage() {
-        const percent = parseFloat(target.style.height.replace('%', '')).toFixed(2) + '%';
-        console.info('style changed!', percent, title);
-        title.innerText = `Stealth ${percent}`;
+        const attPercent = parseFloat(attacker.style.width.replace('%', '')).toFixed(2) + '%';
+        const defPercent = parseFloat(defender.style.width.replace('%', '')).toFixed(2) + '%';
+        console.info('style changed!', attPercent, defPercent, title);
+        title.innerText = `HP ${attPercent} vs ${defPercent}`;
     }
     var observer = new MutationObserver(() => updatePercentage());
     updatePercentage();
-    observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+    observer.observe(attacker, { attributes : true, attributeFilter : ['style'] });
+    observer.observe(defender, { attributes : true, attributeFilter : ['style'] });
 }
 
 function addTitleWrapper(title, className) {
@@ -29,7 +31,6 @@ function addTitleWrapper(title, className) {
         const wrapper = document.createElement('span');
         wrapper.classList.add(className);
         title.appendChild(wrapper);
-        return wrapper;
     } else {
         title.classList.add(toggleClass);
         title.innerHTML = `Attacking - <span class="${className}"></span>`;
@@ -42,11 +43,12 @@ function addTitleWrapper(title, className) {
     const maxLoop = 10000;
     let counter = 1;
     let loop = setInterval(function () {
-        const bar = document.querySelector(`div[class^='stealthBarWrap_'] [class^='level_']`);
+        const defender = document.querySelector(`div#defender [class^='header_'] [class^='progress_']`);
+        const attacker = document.querySelector(`div#attacker [class^='header_'] [class^='progress_']`);
         const title = document.querySelector(`div[class^='titleContainer_'] [class^='title_']`);
-        if (bar && title) {
-            const wrapped = addTitleWrapper(title, 'stealth');
-            watchPercentage(bar, wrapped);
+        if (defender && attacker && title) {
+            const wrapped = addTitleWrapper(title, 'hp');
+            watchPercentages(attacker, defender, wrapped);
             return clearInterval(loop);
         }
         if (counter > maxLoop) {
